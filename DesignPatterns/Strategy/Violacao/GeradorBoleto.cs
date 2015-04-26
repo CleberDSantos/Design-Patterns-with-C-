c75a50474b19d8d15a0d4cb851f62e7b142ad2e7
CleberDS <systemofcleber@gmail.com>
2015-04-26 15:16:49 -0300

    Implementação de violacao do principio

---
+++ DesignPatterns/Strategy/Violacao/GeradorBoleto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Violacao
+{
+    public class GeradorBoleto
+    {
+
+        /// <summary>
+        /// Codigo responsavel por gerar boletos
+        /// </summary>
+        /// <param name="banco">Banco necessario para gerar o boleto</param>
+        /// <returns>Codigo de barras gerado</returns>
+        public string GerarBoleto(string banco)
+        {
+            var codigo = string.Empty;
+
+            if (banco == "ITAU")
+            {
+                codigo = this.GerarBoletoITAU();
+
+            }
+            else if (banco == "Brasil")
+            {
+                codigo = this.GerarBoletoBancoBrasil();
+
+            }
+            else if (banco == "Bradesco")
+            {
+                codigo = this.GerarBancoBradesco();
+            }
+
+            return codigo;
+        }
+
+        /// <summary>
+        /// metodo responsavel pela geracao do codigo de barras do Banco Itau
+        /// </summary>
+        /// <returns>codigo de barras gerado</returns>
+        public string GerarBoletoITAU()
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// metodo responsavel pela geracao do codigo de barras do Banco do Brasil
+        /// </summary>
+        /// <returns>codigo de barras gerado</returns>
+        public string GerarBoletoBancoBrasil()
+        {
+            return string.Empty;
+        }
+
+
+        /// <summary>
+        /// metodo responsavel pela geracao do codigo de barras do Banco Bradesco
+        /// </summary>
+        /// <returns>codigo de barras gerado</returns>
+        public string GerarBancoBradesco()
+        {
+            return string.Empty;
+        }
+
+    }
+}
